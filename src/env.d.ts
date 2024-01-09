7bac0cabd10eb1d67f202006cde38e0d79854ab2
Henry Kroeger <7549541+henrykroeger@users.noreply.github.com>
2024-01-08 23:07:35 -0800

    add typechecking to netlify deploy

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+import 'astro/astro-jsx'
+
+declare global {
+    namespace JSX {
+        type Element = HTMLElement
+        // type Element = astroHTML.JSX.Element // We want to use this, but it is defined as any.
+        type IntrinsicElements = astroHTML.JSX.IntrinsicElements
+    }
+}
